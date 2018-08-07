0c581bd91d518aadfc1c42c33c6bb49b975ba764
Brian Alejandro Mireles Ortiz <1630273@gmail.com>
2018-08-07 00:46:21 -0500

    login

---
+++ JBTienda/JBTienda/FormLogin.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace JBTienda
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.PictureBox pictureBox2;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            System.Windows.Forms.PictureBox pictureBox1;
+            this.mostrar = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Registrarse = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            pictureBox2 = new System.Windows.Forms.PictureBox();
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            pictureBox2.Location = new System.Drawing.Point(119, 224);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(24, 24);
+            pictureBox2.TabIndex = 27;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            pictureBox1.Location = new System.Drawing.Point(119, 168);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(24, 24);
+            pictureBox1.TabIndex = 26;
+            pictureBox1.TabStop = false;
+            // 
+            // mostrar
+            // 
+            this.mostrar.AutoSize = true;
+            this.mostrar.BackColor = System.Drawing.Color.Transparent;
+            this.mostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mostrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mostrar.Location = new System.Drawing.Point(149, 263);
+            this.mostrar.Name = "mostrar";
+            this.mostrar.Size = new System.Drawing.Size(152, 19);
+            this.mostrar.TabIndex = 25;
+            this.mostrar.Text = "Mostrar Contraseña";
+            this.mostrar.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(72, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 33);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "INICIAR SESIÓN";
+            // 
+            // Registrarse
+            // 
+            this.Registrarse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Registrarse.BackColor = System.Drawing.Color.DarkMagenta;
+            this.Registrarse.CausesValidation = false;
+            this.Registrarse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Registrarse.FlatAppearance.BorderSize = 0;
+            this.Registrarse.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Navy;
+            this.Registrarse.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumOrchid;
+            this.Registrarse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Registrarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registrarse.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Registrarse.Location = new System.Drawing.Point(34, 318);
+            this.Registrarse.Name = "Registrarse";
+            this.Registrarse.Size = new System.Drawing.Size(129, 28);
+            this.Registrarse.TabIndex = 23;
+            this.Registrarse.Text = "Registrarse";
+            this.Registrarse.UseVisualStyleBackColor = false;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnIngresar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIngresar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnIngresar.BackColor = System.Drawing.Color.DarkMagenta;
+            this.btnIngresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnIngresar.CausesValidation = false;
+            this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIngresar.FlatAppearance.BorderSize = 0;
+            this.btnIngresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumOrchid;
+            this.btnIngresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumOrchid;
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.ForeColor = System.Drawing.Color.White;
+            this.btnIngresar.Location = new System.Drawing.Point(202, 318);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(100, 29);
+            this.btnIngresar.TabIndex = 22;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseMnemonic = false;
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(149, 228);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(121, 20);
+            this.txtContraseña.TabIndex = 21;
+            this.txtContraseña.UseSystemPasswordChar = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(149, 172);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(122, 20);
+            this.txtUsuario.TabIndex = 20;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrchid;
+            this.ClientSize = new System.Drawing.Size(362, 450);
+            this.Controls.Add(pictureBox2);
+            this.Controls.Add(pictureBox1);
+            this.Controls.Add(this.mostrar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Registrarse);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "FormLogin";
+            ((System.ComponentModel.ISupportInitialize)(pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox mostrar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Registrarse;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+    }
+}
+
